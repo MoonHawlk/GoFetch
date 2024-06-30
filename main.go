7b063c52278bc8f2f54f10de7fce3cc35064af8c
MoonHawlk <filipemorenoprogramador@gmail.com>
2024-06-30 13:33:01 -0300

    Mac OS more information update

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os/exec"
 	"strings"
 
 	"github.com/fatih/color"
-	"github.com/shirou/gopsutil/host" // Deprecated, needs to be changed
+	"github.com/shirou/gopsutil/disk"
+	"github.com/shirou/gopsutil/host"
 	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/net"
 )
 
 func getCPUInfo() (string, error) {
@@ -36,6 +39,14 @@ func getGPUInfo() (string, error) {
 	return "Unknown GPU", nil
 }
 
+func getMacOSVersion() (string, error) {
+	output, err := exec.Command("sw_vers", "-productVersion").Output()
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(output)), nil
+}
+
 func formatLine(label, value string, colorFunc func(a ...interface{}) string) string {
 	return fmt.Sprintf("%s: %s", label, colorFunc(value))
 }
@@ -48,6 +59,8 @@ func generateWindow(lines []string, colorFunc func(a ...interface{}) string) {
 		}
 	}
 
+	maxLength += 2 // Add a small buffer
+
 	horizontalBorder := strings.Repeat("-", maxLength)
 
 	fmt.Println(colorFunc("+" + horizontalBorder + "+"))
@@ -58,6 +71,9 @@ func generateWindow(lines []string, colorFunc func(a ...interface{}) string) {
 }
 
 func main() {
+	detail := flag.Bool("detail", false, "Exibir status detalhado")
+	flag.Parse()
+
 	info, err := host.Info()
 	if err != nil {
 		fmt.Println("Erro ao obter informações do sistema:", err)
@@ -100,6 +116,34 @@ func main() {
 		formatLine("Used Memory", fmt.Sprintf("%v MB", memInfo.Used/1024/1024), cyan),
 		formatLine("CPU", cpuInfo, cyan),
 		formatLine("GPU", gpuInfo, cyan),
+	}
+
+	if *detail {
+		macOSVersion, err := getMacOSVersion()
+		if err != nil {
+			fmt.Println("Erro ao obter versão do macOS:", err)
+			return
+		}
+
+		diskUsage, err := disk.Usage("/")
+		if err != nil {
+			fmt.Println("Erro ao obter uso do disco:", err)
+			return
+		}
+
+		netIOCounters, err := net.IOCounters(false)
+		if err != nil {
+			fmt.Println("Erro ao obter informações de rede:", err)
+			return
+		}
+
+		lines = append(lines, []string{
+			formatLine("macOS Version", macOSVersion, cyan),
+			formatLine("Disk Total", fmt.Sprintf("%v GB", diskUsage.Total/1024/1024/1024), cyan),
+			formatLine("Disk Used", fmt.Sprintf("%v GB", diskUsage.Used/1024/1024/1024), cyan),
+			formatLine("Network Sent", fmt.Sprintf("%v MB", netIOCounters[0].BytesSent/1024/1024), cyan),
+			formatLine("Network Received", fmt.Sprintf("%v MB", netIOCounters[0].BytesRecv/1024/1024), cyan),
+		}...)
 	}
 
 	generateWindow(lines, green)
